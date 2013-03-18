8ba05567aa5806a867f23d4eb9725d610589f764
srusskih <smilemouse2002@ukr.net>
2013-03-18 16:31:52 +0200

    update admin.py:  - code rework  - comment not used for now

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,15 +1,16 @@
 from django.conf import settings
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 from django.contrib import admin
 from django.core.mail import EmailMessage
 from django.core.urlresolvers import reverse
-from django.http import Http404
-from django.shortcuts import redirect
+from django.http import HttpResponseForbidden
+from django.shortcuts import redirect, get_object_or_404
 from django.template.loader import render_to_string
 from django.utils.functional import update_wrapper
 from django.utils.translation import ugettext_lazy as _
-from models import (Newsletter, NewsletterItem, NewsletterAdd,
-                    ExternalSubscriber)
+
+from .models import (Newsletter, NewsletterItem, NewsletterAdd,
+                     ExternalSubscriber)
 
 
 class NewsletterItemInline(admin.StackedInline):
@@ -21,20 +22,15 @@ class NewsletterAddInline(admin.StackedInline):
 
 
 def copy_newsletter(modeladmin, request, queryset):
-    newsletter_fields = Newsletter._meta.get_all_field_names()
-    newsletter_fields.remove('id')
-    newsletter_fields.remove('status')
-    newsletter_fields.remove('newsletteradd')
-    newsletter_fields.remove('newsletteritem')
-
-    for i in queryset:
-        newsletter = Newsletter()
-        for field in newsletter_fields:
-            setattr(newsletter, field, getattr(i, field))
-        newsletter.subject = newsletter.subject + " (copy)"
+    """ create copy of selected newsletters """
+    for original_newsletter in queryset:
+        newsletter = original_newsletter
+        newsletter.id = None
+        newsletter.status = Newsletter.NEW
+        newsletter.subject += " (copy)"
         newsletter.save()
 
-        for item in i.newsletteritem_set.all():
+        for item in original_newsletter.newsletteritem_set.all():
             NewsletterItem.objects.create(
                 newsletter=newsletter,
                 order=item.order,
@@ -46,34 +42,32 @@ def copy_newsletter(modeladmin, request, queryset):
                 picture=item.picture
             )
 
-        for add_ in i.newsletteradd_set.all():
+        for add_ in original_newsletter.newsletteradd_set.all():
             NewsletterAdd.objects.create(
                 newsletter=newsletter,
                 order=add_.order,
                 adds=add_.adds,
                 url=add_.url
             )
-copy_newsletter.short_description = "Make a copy"
 
 
-class NewsletterAdmin(admin.ModelAdmin):
-    list_display = ['subject', 'send_to', 'publication_date', 'status',
-                    'send', 'test_send']
-    list_filter = ['status']
-    inlines = (NewsletterItemInline, NewsletterAddInline)
-    actions = [copy_newsletter]
+copy_newsletter.short_description = _("Make a copy")
 
+
+class SendingMixin(object):
     def test_send(self, obj):
         url = reverse('admin:send_admin', args=[obj.pk])
         return u'<a href="%s">%s</a>' % (url, _('Send admin'))
+
     test_send.allow_tags = True
     test_send.short_description = _("Sent Mail to Admin")
 
     def send(self, obj):
-        if obj.status == Newsletter.NEW:
+        if obj.status == self.model.NEW:
             url = reverse('admin:add_to_queue', args=[obj.pk])
             return u'<a href="%s">%s</a>' % (url, _('Send'))
         return ""
+
     send.allow_tags = True
     send.short_description = _("Add to Queue")
 
@@ -81,22 +75,23 @@ class NewsletterAdmin(admin.ModelAdmin):
         def wrap(view):
             def wrapper(*args, **kwargs):
                 return self.admin_site.admin_view(view)(*args, **kwargs)
+
             return update_wrapper(wrapper, view)
-        urlpatterns = super(NewsletterAdmin, self).get_urls()
+
+        urlpatterns = super(SendingMixin, self).get_urls()
         urlpatterns = patterns('',
-            url(r'^(.+)/send_admin/$',
-                wrap(self.send_admin),
-                name='send_admin'),
-            url(r'^(.+)/add_to_queue/$',
-                wrap(self.add_to_queue),
-                name='add_to_queue'),
+                               url(r'^(.+)/send_admin/$', wrap(self.send_admin),
+                                   name='send_admin'),
+                               url(r'^(.+)/add_to_queue/$',
+                                   wrap(self.add_to_queue),
+                                   name='add_to_queue'),
         ) + urlpatterns
         return urlpatterns
 
     def send_admin(self, request, object_id):
         opts = self.model._meta
         user = request.user
-        letter = self._get_object(request, object_id)
+        letter = get_object_or_404(self.queryset(request), id=object_id)
         context = {
             'user': user,
             'obj': letter,
@@ -108,47 +103,54 @@ class NewsletterAdmin(admin.ModelAdmin):
                              settings.DEFAULT_FROM_EMAIL, [user.email])
         email.content_subtype = 'html'
         email.send()
-        return redirect('admin:%s_%s_changelist' % (opts.app_label, opts.module_name))
+        return redirect(
+            'admin:%s_%s_changelist' % (opts.app_label, opts.module_name)
+        )
 
     def add_to_queue(self, request, object_id):
-        opts = self.model._meta
-        self.model.objects.filter(id=object_id).update(status=Newsletter.QUEUED)
-        return redirect('admin:%s_%s_changelist' % (opts.app_label, opts.module_name))
+        """ push newsletters to queue """
+        # change object permissions required
+        if not self.has_change_permission(request):
+            return HttpResponseForbidden("Not enough permissions")
 
-    def _get_object(self, request, object_id):
-        try:
-            obj = self.queryset(request).get(pk=object_id)
-        except self.model.DoesNotExist:
-            obj = None
-        if not self.has_change_permission(request, obj):
-            raise Http404('No permission')
-        if obj is None:
-            raise Http404(
-                '%(name)s object with primary key %(key)r does not exist.' %
-                {'name': self.model._meta.verbose_name, 'key': object_id}
-            )
-        return obj
+        opts = self.model._meta
+        self.queryset(request).filter(id=object_id). \
+            update(status=Newsletter.QUEUED)
+        return redirect(
+            'admin:%s_%s_changelist' % (opts.app_label, opts.module_name)
+        )
+
+
+class NewsletterAdmin(SendingMixin, admin.ModelAdmin):
+    list_display = ['subject', 'send_to', 'publication_date', 'status',
+                    'send', 'test_send']
+    list_filter = ['status']
+    inlines = (NewsletterItemInline, NewsletterAddInline)
+    actions = [copy_newsletter]
 
 
 class ExternalSubscriberAdmin(admin.ModelAdmin):
     list_display = ['email', 'first_name', 'last_name', 'is_subscribed']
-    list_filter = ['is_subscribed',]
+    list_filter = ['is_subscribed']
     search_fields = ['email', 'first_name', 'last_name']
-
-    def get_urls(self):
-        from admin_views import import_subscribers
-
-        def wrap(view):
-            def wrapper(*args, **kwargs):
-                return self.admin_site.admin_view(view)(*args, **kwargs)
-            return update_wrapper(wrapper, view)
-        urlpatterns = super(ExternalSubscriberAdmin, self).get_urls()
-        urlpatterns = patterns('',
-            url(r'^import/$',
-                wrap(import_subscribers),
-                name='externalsubscriber_import'),
-        ) + urlpatterns
-        return urlpatterns
+    #
+    # def get_urls(self):
+    #     from admin_views import import_subscribers
+    #
+    #
+    #     def wrap(view):
+    #         def wrapper(*args, **kwargs):
+    #             return self.admin_site.admin_view(view)(*args, **kwargs)
+    #
+    #         return update_wrapper(wrapper, view)
+    #
+    #     urlpatterns = super(ExternalSubscriberAdmin, self).get_urls()
+    #     urlpatterns = patterns('',
+    #                            url(r'^import/$',
+    #                                wrap(import_subscribers),
+    #                                name='externalsubscriber_import'),
+    #     ) + urlpatterns
+    #     return urlpatterns
 
 
 admin.site.register(Newsletter, NewsletterAdmin)
